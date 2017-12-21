a470741045b3a55697899db00b979dbdf1f0d324
binio <tbiniecki@gmail.com>
2017-12-21 17:05:00 +0000

    add event listeners

--- BudgetApp/app.js
+++ BudgetApp/app.js
@@ -1,27 +1,30 @@
-//lets add modules a
+//Budget Controller
 var budgetController = (function(){
-    var x = 23;
-    var add = function(a) {
-        return x+a;
-    }
-
-    return {
-        publicTest: function(b){
-            return add(b);
-        }
-    }
 
 })();
 
+//UI Controller
 var UIController = (function(){
 
 })();
 
+//App Controller
 var controller = (function(budgetCtrl, UICtrl){
-    var z = budgetCtrl.publicTest(7);
-    return {
-        anotherPublic: function(){
-            console.log(z);
-        }
+
+    var ctrlAddItem = function(){
+        //1.get the field input data
+        //2.add item to the budget controller
+        //3.add item to UI
+        //4.calculate budget
+        //5.display budget
+        console.log('it works!');
     }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event){
+        if(event.charCode === 13 || event.which === 13){
+            ctrlAddItem();
+        }
+    });
 })(budgetController, UIController);
