11a4818d4e8c5277e03f8ef3436bae7e7a12a730
binio <tbiniecki@gmail.com>
2017-12-22 15:14:01 +0000

    add controller method to handle data and item ID generation

--- BudgetApp/app.js
+++ BudgetApp/app.js
@@ -1,8 +1,55 @@
 //Budget Controller
 var budgetController = (function(){
 
+    var Expense = function(id,description,value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var Income = function(id,description,value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var getNewId = function(type){
+        //get value of lastID object type and increase it by 1
+        if(data.allItems[type].length === 0){
+            return 0;
+        }
+        var lastElem = data.allItems[type].length - 1;
+        return data.allItems[type][lastElem].id + 1;
+    };
+
+    var data = {
+        allItems:{
+            inc:[],
+            exp:[]
+        },
+        totals:{
+            exp:0,
+            inc:0
+        }
+    };
+    
+    return {
+      addItem: function (type, des, val) {
+          var newItem;
+
+          if(type==='exp') {
+              newItem = new Expense(getNewId(type), des, val);
+          } else if(type==='inc') {
+              newItem = new Income(getNewId(type), des, val);
+          }
+          data.allItems[type].push(newItem);
+          console.log(data.allItems);
+      }  
+    };
+
 })();
 
+//--------------------------------------------------------------------------------
 //UI Controller
 var UIController = (function(){
     var DOMStrings = {
@@ -25,6 +72,8 @@ var UIController = (function(){
     };
 })();
 
+
+//--------------------------------------------------------------------------------
 //App Controller
 var controller = (function(budgetCtrl, UICtrl){
 
@@ -45,6 +94,7 @@ var controller = (function(budgetCtrl, UICtrl){
         var input = UIController.getInput();
         console.log(input);
         //2.add item to the budget controller
+        budgetController.addItem(input.type, input.description, input.value);
         //3.add item to UI
         //4.calculate budget
         //5.display budget
